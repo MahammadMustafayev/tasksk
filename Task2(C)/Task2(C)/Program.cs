7f5630b31db7f8952ee86844a897c60b597e67b2
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-04 07:57:04 +0300

    added

---
+++ Task2(C)/Task2(C)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task2_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            switch (month)
+            {
+
+                case"Yanvar":
+                case"Mart":
+                case"May":
+                case"Iyul":
+                case"Avqust":
+                case"Oktyabr":
+                case"Dekabr":
+            Console.WriteLine("Bu ayda 31 gün var.");
+            break;
+
+
+            case"Aprel":
+            case"Iyun":
+                case"Sentyabr":
+                case"Noyabr":
+            Console.WriteLine("Bu ayda 30 gün var");
+            break;
+            
+                 case"Fevral":
+            Console.WriteLine("Bu ayda 28 gün var");
+                    break;
+            
+            default:
+                Console.WriteLine("Bele ay yoxdur");
+            break;
+            }
+        }
+    }
+}
