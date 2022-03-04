b5e456b0290f75f3f861a6329fe50d88bfb6337d
MahammadMustafayev <tu7ekdm78@code.edu.az>
2022-03-04 07:29:01 +0300

    added

---
+++ Task1(C)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int m = 30;
+            for (int n = 3; n<=m ; n++)
+            {
+                if (n % 7 == 0)
+                {
+                    Console.WriteLine(n);
+                }
+                else
+                {
+                    
+                }
+            }
+            
+        }
+    }
+}
